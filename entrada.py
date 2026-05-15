fc3258db691a2d02589116cdda52c1f48fac7716
augustograeml <augusto.graeml@gmail.com>
2026-05-15 17:37:05 -0300

    mudei sla oq

--- entrada.py
+++ entrada.py
@@ -1,9 +1,14 @@
 from itertools import islice
 
-def lerLinha(arquivo, linha_inicio):
-    with open(arquivo, "r", encoding="utf-8") as f:
-        for linha in islice(f, linha_inicio, linha_inicio + 1):
-            entrada = linha
+def lerLinha(file, linha_inicio, final):
+    for linha in islice(file, linha_inicio, final):
+        entrada = linha
+    entrada = entrada.split(",")
+    del entrada[1]
+    del entrada[1]
     return entrada
 
-
+'''
+with open('data/02_treino_sinais_vitais_com_label.txt','r',encoding='UTF-8') as f:
+    print(lerLinha(f,1,2))
+'''
