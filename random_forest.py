66ea9132b09b53ea692556b99c058f2c8d09717f
GustavoBuenoAdB <gustavobuenodacosta@gmail.com>
2026-05-15 10:41:31 -0300

    adicionando esqueleto comentado de rand_forst, mas sem implementar nada, só pra ter a ideia

--- random_forest.py
+++ random_forest.py
@@ -1,1 +1,29 @@
 #tem que fazer né...
+
+N_ENTRADAS = 6
+N_ARVORES = 10
+
+class Arvore:
+	def __init__(self, n_atributos, entradas):
+		# escolhe uma hierarquia de atributos usando a entropia das entradas
+		# cria a arvore baseada nesta hierarquia
+
+	def processa(self, entrada):
+		# só descer raiz abaixo e ver se resulta em True ou False e retorna esse voto.
+
+class Floresta:
+	def __init__(self):
+		self.arvores = []
+		# cria uma penca de arvores, (N_ARVORES)
+	
+	def processa(self, entrada):
+		# manda cada arvore processar aquela entrada
+		# contabiliza os votos e retorna
+
+def main():
+	# cria uma floresta, manda processar coisas. 
+	return 0
+
+
+if __name__ == "__main__":
+	main()
