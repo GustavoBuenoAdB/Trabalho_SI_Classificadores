bee72130e47a274ac716946c193ea0d5dd443832
augustograeml <augusto.graeml@gmail.com>
2026-05-18 00:35:33 -0300

    tem algumas coisas funcionando mas não muitas

--- MLP.py
+++ MLP.py
@@ -1,7 +1,12 @@
-
+import entrada as ent
 import random
+import numpy
 from scipy.special import expit #exipit é a sigmoid
 
+N_ENTRADAS_TREINO = 50
+N_EPOCAS = 5
+TAM_MINI = 10
+TAXA_APRENDIZAGEM = 0.01
 #numeros de entrada e saidas de cada camada
 N_ENT_C1 = 3
 N_NEU_C1 = 4
@@ -10,23 +15,32 @@ N_ENT_C2 = 4
 N_NEU_C2 = 6
 
 N_ENT_C3 = 6
-N_NEU_C3 = 2
+N_NEU_C3 = 4
 
-#flags de "debug" = printf
-DEBUG_CAM = False
-DEBUG_NEU = True
+#flags de "debug" = print
+DEBUG_CAM = True
+DEBUG_NEU = False
+
+saida_esperada = [[1.00,0.00,0.00,0.00],[0.00,1.00,0.00,0.00],[0.00,0.00,1.00,0.00],[0.00,0.00,0.00,1.00]]
 
 def f_ativacao(x):
 	return expit(x)
+
+def derivada_sigmoid(x):
+    s = f_ativacao(x)
+    return s * (1 - s)
 
 class Neuronio:
 	def __init__(self, n_entradas):
 		self.n_entradas = n_entradas
 		self.pesos = []
+		self.ultima_entrada = []
+		self.ultima_ativacao = []
+		self.ultima_som = []
 		self.vies = random.random()
 		# inicializa os pesos aleatoriamente
 		for i in range (n_entradas + 1):
-			self.pesos.append(random.random())
+			self.pesos.append(random.uniform(-0.5, 0.5))
 
 
 	def processa(self, entradas):
@@ -34,13 +48,21 @@ class Neuronio:
 		n = len(entradas)
 		for i in range(1, n + 1):
 			som += entradas[i-1] * self.pesos[i]		
-		som -= self.pesos[0] # esse aqui é o vies (indice 0 do vetor de pesos)
-
+		som += self.pesos[0] # esse aqui é o vies (indice 0 do vetor de pesos)
+		self.ultima_som = som
+		self.ultima_entrada = entradas
 		if (DEBUG_NEU):
 			print(f" - entrada: {entradas} \n - pesos: {self.pesos} \n - soma: {som}")
+		ativacao = f_ativacao(som)
+		self.ultima_ativacao = ativacao #guardando a anterior para utlilzat no backprop
+		return ativacao
+	
+	def atualiza_pesos(self, delta, taxa):
+		self.pesos[0] = self.pesos[0] - taxa * delta  # atualiza vies
+		for i in range(self.n_entradas):
+			self.pesos[i+1] -= taxa * delta * self.ultima_entrada[i] #pq i -1? AUGUSTO TODO
 
-		return f_ativacao(som)
-
+	
 class Camada:
 	def __init__(self, n_entradas, n_saidas):
 		self.n_entradas = n_entradas
@@ -66,7 +88,35 @@ class Camada:
 			print(f" - entrada: {entrada} \n - saida: {saida}")
 
 		return saida
+	
+	def backprop(self, deltas_frente, camada_frente, taxa):
+		deltas = []
+		for i, neuronio in enumerate(self.neuronios):
+			erro = 0
+			for j, neuronio_frente in enumerate(camada_frente.neuronios):
+				erro += deltas_frente[j] * neuronio_frente.pesos[i+1]
+			
+			delta = erro * derivada_sigmoid(neuronio.ultima_som)
+			neuronio.atualiza_pesos(delta, taxa)
+			deltas.append(delta)
+		
+		return deltas
 
+	def backprop_saida(self, saida, esperado, taxa):
+		deltas = []
+		for i in range(N_NEU_C3):
+			erro = saida[i] - saida_esperada[esperado][i]
+			delta = self.calcula_delta(erro,i)
+			self.neuronios[i].atualiza_pesos(delta, taxa)
+			deltas.append(delta)
+		return deltas
+	
+	def calcula_delta(self, erro, indx):
+		#delta = derivada do custo * derivada da sigmoide
+		d_sigmoide = derivada_sigmoid(self.neuronios[indx].ultima_som) 
+		delta = erro * d_sigmoide
+		return delta
+	
 class MLP:
 	def __init__(self):
 		self.camadas = []
@@ -82,6 +132,25 @@ class MLP:
 			saida = (self.camadas[i].processa(saida)) # enquanto tiver camada a saida de um é a entrada dotro
 
 		return saida
+	'''
+	def calcula_custo(entrada,saida):
+		esperado = saida_esperada[entrada[N_NEU_C3]]
+		custo = []
+		for i in range(N_NEU_C3):
+			custo.append((saida[i] - esperado[i])*(saida[i] - esperado[i])) 
+		media_custo = sum(custo) / len(custo)
+		return media_custo
+	'''
+	def uma_epoca(self,saida,esperado):
+		deltas = self.camadas[2].backprop_saida(saida, esperado, TAXA_APRENDIZAGEM)
+		print(deltas)
+		for i in range(len(self.camadas)-2,0,-1):
+			deltas = self.camadas[i].backprop(deltas,self.camadas[i+1],TAXA_APRENDIZAGEM)
+	
+	def treina(self,):
+		#aqui precisa orquestrar o treinamento em cada mini pedaço de treino para cada época
+		return 1
+
 
 def main():
 	mlp = MLP()
@@ -89,13 +158,21 @@ def main():
 	mlp.add_camada(Camada(N_ENT_C1, N_NEU_C1))
 	mlp.add_camada(Camada(N_ENT_C2, N_NEU_C2))
 	mlp.add_camada(Camada(N_ENT_C3, N_NEU_C3))
+	print(f"Esses são os pesos iniciais: {mlp.camadas[1].neuronios[1].pesos}")
+	
+	f = open('data/02_treino_sinais_vitais_com_label.txt', 'r', encoding='UTF-8')
+	dados = ent.lerEntradas(f, 0, N_ENTRADAS_TREINO)
 
-	print(mlp.processa((-2, 1, 3)))
-
+	for i in dados:
+		saida = mlp.processa((i[1],i[2],i[3]))
+		print(f"classe esperada: {i[5]} | saida: {saida}")
+		mlp.uma_epoca(saida,i[5]-1) #-1 para evitar que ele tente acessar o indice 4 quando a classe for 4 e quebre
+	
 if __name__ == "__main__":
 	main()
 
 
-# TODO: aqui ta implementada só a base da base, não treina nem pega de arquivo, ainda é bem xunxa, n sei inclusive se o vies o - la ta sendo calculado certinho
+# TODO: aqui ta implementada só a base da base, não uma_epoca
+#nem pega de arquivo, ainda é bem xunxa, n sei inclusive se o vies o - la ta sendo calculado certinho
 # pra ser sincero meio que esqueci dele, tem que ver isso, adicionar leitura dos dados certinhos la, e programar a tal da BACKPROPAGATION, mas aqui é só 
 # o esquletinho pra brincar de ajustar parametro de camada
